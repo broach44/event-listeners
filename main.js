3059c337b310858d8346d88da3e97224d1f51b24
Crystal Broach <crysland@gmail.com>
2019-08-27 21:32:33 -0500

    created buttons with event listeners to filter cards

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const printToDom = (toPrint, divId) => {
-    document.getElementById(divId).innerHTML += toPrint
+    document.getElementById(divId).innerHTML = toPrint
 }
 
 const pies = [
@@ -36,9 +36,10 @@ const pies = [
 ]
 
 const printPies = (pieArr) => {
+    let piesToPrint= ''
     for (let i = 0; i < pieArr.length; i++) {
         const pieType = pieArr[i];
-        const piesToPrint = 
+        piesToPrint += 
         `
         <div class="pie-card">
         <h1>${pieType.pieName}</h1>
@@ -46,8 +47,9 @@ const printPies = (pieArr) => {
         <p class="instructor">${pieType.instructorFirstName}</p>
         </div>
         `
-        printToDom(piesToPrint, 'container');
+        
     }
+    printToDom(piesToPrint, 'container');
 }
 
 
@@ -61,6 +63,87 @@ const printPies = (pieArr) => {
 // })
 // You will not be able to call a function within the event listener (first parens); call after bracket
 
-printPies(pies);
+// printPies(pies);
+
+// document.getElementById('zoe-button').addEventListener('click', () => {
+// //figure out Who this instructor is for the button we clicked on
+//     const instructor = 'zoe';
+// //only get those pies from the list of all the pies
+//     const zoesPies = []
+//     for (let i = 0; i < pies.length; i++) {
+//         const pie = pies[i];
+//         if (pie.instructorFirstName === instructor) {
+//             zoesPies.push(pie);
+//         }
+//     }
+// //smaller list of pies back into the printPies function
+
+//     printPies(zoesPies)
+// })
+
+// document.getElementById('michael-button').addEventListener('click', () => {
+//     //figure out Who this instructor is for the button we clicked on
+//         const instructor = 'michael';
+//     //only get those pies from the list of all the pies
+//         const michaelsPies = []
+//         for (let i = 0; i < pies.length; i++) {
+//             const pie = pies[i];
+//             if (pie.instructorFirstName === instructor) {
+//                 michaelsPies.push(pie);
+//             }
+//         }
+//     //smaller list of pies back into the printPies function
+    
+//         printPies(michaelsPies)
+//     })
+
+const buttonClick = (e) => {
+    const instructor = e.target.id
+    const selectedPies = []
+    for (let i = 0; i < pies.length; i++) {
+        const pie = pies[i];
+        if (pie.instructorFirstName === instructor) {
+            selectedPies.push(pie);
+        }
+    }
+    printPies(selectedPies)
+}
+
+    document.getElementById('callan').addEventListener('click', (e) => {
+        const instructor = e.target.id
+
+
+        // figure out Who this instructor is for the button we clicked on
+
+        //only get those pies from the list of all the pies
+            const selectedPies = []
+            for (let i = 0; i < pies.length; i++) {
+                const pie = pies[i];
+                if (pie.instructorFirstName === instructor) {
+                    selectedPies.push(pie);
+                }
+            }
+            printPies(selectedPies)
+        })
+
+        document.getElementById('michael').addEventListener('click', (e) => {
+            const instructor = e.target.id
+                const selectedPies = []
+                for (let i = 0; i < pies.length; i++) {
+                    const pie = pies[i];
+                    if (pie.instructorFirstName === instructor) {
+                        selectedPies.push(pie);
+                    }
+                }
+                printPies(selectedPies)
+            })
+
+        document.getElementById('zoe').addEventListener('click', buttonClick)
+
+        document.getElementById('all').addEventListener('click', (e) => {
+                printPies(pies)
+            })
+
+
 
 
